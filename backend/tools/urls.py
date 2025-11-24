c459a3ee4c2dff8ce58e0dd39e0ef86be33262fb
Arturo Rodriguez <arodriguezr@cdmx.gob.mx>
2025-11-24 14:03:10 -0600

    feat(urls): Definicion de las rutas de cada funcion dentro del backoffice (Backend)

--- backend/tools/urls.py
+++ backend/tools/urls.py
@@ -1,6 +1,22 @@
 from django.urls import path
-from .views import HashGenerator
+from .views import (
+    TagScraperView,
+    FormatterView,
+    HashConverterView,
+    JwtGeneratorView,
+    TestMatrixGeneratorView,
+    SiteCheckerView,
+    ToolLogListView,
+    ToolLogDeleteView
+)
 
 urlpatterns = [
-    path('hash/', HashGenerator.as_view(), name='hash-generator'),
+    path('scrape-tags/', TagScraperView.as_view(), name='scrape-tags'),
+    path('format/', FormatterView.as_view(), name='formatter'),
+    path('hash/', HashConverterView.as_view(), name='hash-converter'),
+    path('jwt/', JwtGeneratorView.as_view(), name='jwt-generator'),
+    path('test-matrix/', TestMatrixGeneratorView.as_view(), name='test-matrix'),
+    path('site-checker/', SiteCheckerView.as_view(), name='site-checker'),
+    path("logs/", ToolLogListView.as_view(), name="logs"),
+    path("logs/<int:log_id>/", ToolLogDeleteView.as_view(), name="logs-delete"),
 ]
