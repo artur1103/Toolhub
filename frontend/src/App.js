cab39de5ed101c46d9c2d6e4c07fa153fca63231
Arturo Rodriguez <arodriguezr@cdmx.gob.mx>
2025-11-24 14:07:01 -0600

    feat(App): Se exponen las rutas necesarias para el fronted (Fronted)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,32 @@
+// src/App.js
 import React from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
-import Tools from "./pages/Tools";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Backoffice from "./pages/Backoffice";
+import WebScraperTool from "./pages/WebScraperTool";
+import FormatterTool from "./pages/FormatterTool";
+import HashTool from "./pages/HashTool";
+import JwtTool from "./pages/JwtTool";
+import TestMatrixTool from "./pages/TestMatrixTool";
+import SiteCheckerTool from "./pages/SiteCheckerTool";
+import AuditLogs from "./pages/AuditLogs";
+
 
 function App() {
   return (
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<Tools />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Backoffice />} />
+          <Route path="/tools/scraper" element={<WebScraperTool />} />
+          <Route path="/tools/formatter" element={<FormatterTool />} />
+          <Route path="/tools/hash" element={<HashTool />} />
+          <Route path="/tools/jwt" element={<JwtTool />} />
+          <Route path="/tools/test-matrix" element={<TestMatrixTool />} />
+          <Route path="/tools/site-checker" element={<SiteCheckerTool />} />
+          <Route path="/tools/audit" element={<AuditLogs />} />
+        </Routes>
+      </Layout>
     </HashRouter>
   );
 }
