5282ff77a67a6f5a3f8516f8b9c577896a05ef2b
Arturo Rodriguez <arodriguezr@cdmx.gob.mx>
2025-11-24 13:55:05 -0600

    feat(settings): Configuraciones de la API (Backend)

--- backend/toolhub_api/settings.py
+++ backend/toolhub_api/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'toolhub_api.middleware.GlobalRequestLoggerMiddleware',
+
 ]
 
 ROOT_URLCONF = 'toolhub_api.urls'
